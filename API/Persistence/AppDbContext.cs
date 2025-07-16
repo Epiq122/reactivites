b6e77d355e4026db0e51ccca8d548b2c8ec64799
Robert Gleason <l2ob122@gmail.com>
2025-07-15 19:53:18 -0700

    completely changed the folder structure

--- API/Persistence/AppDbContext.cs
+++
@@ -1,9 +0,0 @@
-using Domain;
-using Microsoft.EntityFrameworkCore;
-
-namespace Persistence;
-
-public class AppDbContext(DbContextOptions options) : DbContext(options)
-{
-    public required DbSet<Activity> Activities { get; set; }
-}
